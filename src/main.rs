ec17caf4b250ec8771e278e5d88f85b915f05965
Melody <machineonamission@gmail.com>
2025-08-01 17:01:11 -0500

    lol lmao

---
+++ src/main.rs
@@ -0,0 +1,39 @@
+
+use serenity::async_trait;
+use serenity::model::channel::Message;
+use serenity::prelude::*;
+
+struct Handler;
+
+#[async_trait]
+impl EventHandler for Handler {
+    async fn message(&self, ctx: Context, msg: Message) {
+        let bot_self = ctx.cache.current_user().clone();
+        if msg.mentions.contains(&bot_self) {
+            if let Err(why) = msg.channel_id.say(&ctx.http, "Pong!").await {
+                println!("Error sending message: {why:?}");
+            }
+        }
+        // if msg.mentions
+    }
+}
+#[tokio::main]
+async fn main() {
+    // Login with a bot token from the environment
+    let token = include_str!("../discordkey.txt");
+    // Set gateway intents, which decides what events the bot will be notified about
+    let intents = GatewayIntents::GUILD_MESSAGES
+        | GatewayIntents::DIRECT_MESSAGES
+        | GatewayIntents::MESSAGE_CONTENT;
+
+    // Create a new instance of the Client, logging in as a bot.
+    let mut client = Client::builder(&token, intents)
+        .event_handler(Handler)
+        .await
+        .expect("Err creating client");
+
+    // Start listening for events by starting a single shard
+    if let Err(why) = client.start().await {
+        println!("Client error: {why:?}");
+    }
+}
